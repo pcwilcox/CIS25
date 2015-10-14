50b3f882b517612082499e7af6791af97148c249
Pete Wilcox <petercwilcox@gmail.com>
2015-10-13 18:54:14 -0700

    fleshing out fraction.cpp

--- CIS25Fall2015Driver/CIS25Fall2015Driver/fraction.cpp
+++ CIS25Fall2015Driver/CIS25Fall2015Driver/fraction.cpp
@@ -1,27 +1,81 @@
 #include "fraction.h"
+#include <iostream>
 
+using namespace std;
 
 
 
 Fraction::Fraction() {
+	num = 0;
+	denom = 1;
+	isNegative = false;
 }
 
-Fraction::Fraction(int) {
+Fraction::Fraction(int input) {
+	if (input < 0) {
+		input = -input;
+		isNegative = true;
+	} else {
+		isNegative = false;
+	}
+	num = input;
+	denom = 1;
 }
 
-Fraction::Fraction(const Fraction &) {
+Fraction::Fraction(const Fraction& input) {
+	if (input.getSign == true) {
+		num = -input.getNum;
+	} else {
+		num = input.getNum;
+	}
+
+	denom = input.getDenom;
 }
 
 int Fraction::getNum(void) {
-	return 0;
+	return num;
 }
 
-void Fraction::setNum(int) {
+void Fraction::setNum(int input) {
+	if (input < 0) {
+		num = -input;
+	} else {
+		num = input;
+	}
 }
 
 int Fraction::getDenom(void) {
-	return 0;
+	return denom;
 }
 
-void Fraction::setDenom(int) {
+void Fraction::setDenom(int input) {
+	if (input != 0) {
+		denom = input;
+	} else if (input < 0) {
+		denom = -input;
+		isNegative = true;
+	} else {
+		denom = input;
+	}
+}
+
+// Returns true if negative
+bool Fraction::getSign(void) {
+	return isNegative;
+}
+
+void Fraction::setSign(bool input) {
+	isNegative = input;
+}
+
+char Fraction::printSign() {
+	if (isNegative == true) {
+		return '-';
+	} else {
+		return '+';
+	}
+}
+
+void Fraction::print() {
+	cout << printSign() << num << "/" << denom;
 }
