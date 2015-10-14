8b38ea8c202f994d6d7df0245bc4623b445ae63d
Pete Wilcox <petercwilcox@gmail.com>
2015-10-13 18:30:37 -0700

    fraction.h

---
+++ CIS25Fall2015Driver/CIS25Fall2015Driver/CISFall2015Driver.cpp
@@ -0,0 +1,11 @@
+/*
+ * Program name: CISFall2015Driver.cpp
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	return 0;
+}
---
+++ CIS25Fall2015Driver/CIS25Fall2015Driver/fraction.cpp
@@ -0,0 +1,24 @@
+#include "fraction.h"
+
+Fraction::Fraction() {
+}
+
+Fraction::Fraction(int) {
+}
+
+Fraction::Fraction(const Fraction &) {
+}
+
+int Fraction::getNum(void) {
+	return 0;
+}
+
+void Fraction::setNum(int) {
+}
+
+int Fraction::getDenom(void) {
+	return 0;
+}
+
+void Fraction::setDenom(int) {
+}
