16390d0b87c2be698abca93e4f54f3bc77033a0d
pcwilcox <petercwilcox@gmail.com>
2015-10-23 16:39:10 -0700

    added original code

--- CIS25Fall2015MT1Problem2PeteWilcox.cpp/CIS25Fall2015MT1Problem2PeteWilcox.cpp/CIS25Fall2015MT1Problem2PeteWilcox.cpp
+++ CIS25Fall2015MT1Problem2PeteWilcox.cpp/CIS25Fall2015MT1Problem2PeteWilcox.cpp/CIS25Fall2015MT1Problem2PeteWilcox.cpp
@@ -1,9 +1,9 @@
 /**
-* Program Name: cis25Fall2015MTProblem2PeteWilcox.cpp
-* Discussion:   Mid-Term 1 Problem 2
-* Written by:   Pete Wilcox
-* Date:         2015/10/20
-*/
+ * Program Name: cis25Fall2015MTProblem2PeteWilcox.cpp
+ * Discussion:   Mid-Term 1 Problem 2
+ * Written by:   Pete Wilcox
+ * Date:         2015/10/23
+ */
 
 #include <iostream>
 
@@ -12,11 +12,116 @@ using namespace std;
 int* getUncommonDigitStatsticsPeteWilcox(int*, int);
 void displayHeader(void);
 void displayMenu(void);
-void getInfo(void);
 
 int main() {
+	int menuChoice;
+	int* userArray;
+	int* returnedArray;
+	int arraySize;
+	int evens;
+	int odds;
+	int oddStart;
+	int i = 0;
+
 	displayHeader();
-	displayMenu();
+
+	do {
+		displayMenu();
+
+		cin >> menuChoice;
+
+		switch (menuChoice) {
+		case 1:
+
+			cout << "\n  How many integers? ";
+			cin >> arraySize;
+
+			userArray = new int[arraySize];
+
+			for (i = 0; i < arraySize; i++) {
+				cout << "    Enter integer #" << (i + 1) << ": ";
+				cin >> *(userArray + i);
+			}
+
+			cout << "\n  The original array:";
+
+			for (i = 0; i < arraySize; i++) {
+				cout << "\n    " << *(userArray + i);
+			}
+
+			cout << "\n\n  Calling getUncommonDigitStatsticsPeteWilcox() -";
+
+			returnedArray = getUncommonDigitStatsticsPeteWilcox(userArray,
+				arraySize);
+
+			if (*(returnedArray) > 0) {
+				// Assign evens, odds, oddstart values accordingly
+				if (*(returnedArray + 1) > 0) {
+					evens = *(returnedArray + 1);
+					oddStart = (evens * 2) + 2;
+				} else {
+					evens = 0;
+					oddStart = 2;
+				}
+
+				if (*(returnedArray + oddStart) > 0) {
+					odds = *(returnedArray + oddStart);
+				} else {
+					odds = 0;
+				}
+			}
+
+			cout << "\n\n  Displaying after returning the array -- "
+				"\n    There is/are ";
+			if (odds + evens > 0) {
+				cout << (odds + evens);
+			} else {
+				cout << "no";
+			}
+			cout << " uncommon digit(s)";
+
+
+
+			if (evens > 0) {
+				evens = *(returnedArray + 1);
+				oddStart = evens * 3;
+				cout << "\n\n    There is/are " << evens <<
+					" even uncommon digits.";
+
+				for (i = 2; i < oddStart; i += 2) {
+					cout << "\n      The digit " << *(returnedArray + i)
+						<< " occurs " << *(returnedArray + i + 1)
+						<< " time(s).";
+				}
+			} else {
+				oddStart = 2;
+			}
+
+
+
+			if (odds > 0) {
+				cout << "\n\n    There is/are " << odds <<
+					" odd uncommon digits.";
+				for (i = oddStart + 1; i < *(returnedArray); i += 2) {
+					cout << "\n      The digit " << *(returnedArray + i)
+						<< " occurs " << *(returnedArray + i + 1)
+						<< " time(s).";
+				}
+			}
+
+
+			delete[] userArray;
+			delete[] returnedArray;
+
+			break;
+		case 2:
+			cout << "\n  Fun exercise ...";
+			break;
+		default:
+			cout << "\n  WRONG OPTION!";
+		}
+	} while (menuChoice != 2);
+
 	return 0;
 }
 
@@ -27,123 +132,18 @@ void displayHeader() {
 		"Assignment Information --\n"
 		"  Assignment Number:  Midterm #1 – Problem #2\n"
 		"  Written by:         Pete Wilcox\n"
-		"  Due Date:           2015/10/22\n\n";
+		"  Due Date:           2015/10/23\n\n";
 }
 
 void displayMenu() {
-	int menuChoice;
-	do {
-		cout << "\n****************************************************"
-			"\n*                     MENU                         *"
-			"\n* 1. Calling getUncommonDigitStatsticsPeteWilcox() *"
-			"\n* 2. Quit                                          *"
-			"\n****************************************************"
-			"\nSelect an option (1 or 2): ";
-		cin >> menuChoice;
-
-		switch (menuChoice) {
-		case 1:
-			getInfo();
-			break;
-		case 2:
-			cout << "\n  Fun exercize ...";
-			break;
-		default:
-			cout << "\n  WRONG OPTION!";
-		}
-	} while (menuChoice != 2);
-
+	cout << "\n\n****************************************************"
+		"\n*                     MENU                         *"
+		"\n* 1. Calling getUncommonDigitStatsticsPeteWilcox() *"
+		"\n* 2. Quit                                          *"
+		"\n****************************************************"
+		"\nSelect an option (1 or 2): ";
 }
 
-void getInfo() {
-	int* userArray;
-	int* returnedArray;
-	int arraySize;
-	int evens;
-	int odds;
-	int oddStart;
-	int i = 0;
-
-	cout << "\n  How many integers? ";
-	cin >> arraySize;
-
-	userArray = new int[arraySize];
-
-	for (i = 0; i < arraySize; i++) {
-		cout << "    Enter integer #" << (i + 1) << ": ";
-		cin >> *(userArray + i);
-	}
-
-	cout << "\n  The original array:";
-
-	for (i = 0; i < arraySize; i++) {
-		cout << "\n    " << *(userArray + i);
-	}
-
-	cout << "\n  Calling getUncommonDigitStatsticsPeteWilcox() -";
-
-	returnedArray = getUncommonDigitStatsticsPeteWilcox(userArray,
-		arraySize);
-
-	if (*(returnedArray) > 0) {
-		// Assign evens, odds, oddstart values accordingly
-		if (*(returnedArray + 1) > 0) {
-			evens = *(returnedArray + 1);
-			oddStart = (evens * 2) + 2;
-		} else {
-			evens = 0;
-			oddStart = 2;
-		}
-
-		if (*(returnedArray + oddStart) > 0) {
-			odds = *(returnedArray + oddStart);
-		} else {
-			odds = 0;
-		}
-	}
-
-	cout << "\n\n  Displaying after returning the array -- "
-		"\n    There is/are ";
-	if (odds + evens > 0) {
-		cout << (odds + evens);
-	} else {
-		cout << "no";
-	}
-	cout << " uncommon digit(s)";
-
-
-
-	if (evens > 0) {
-		evens = *(returnedArray + 1);
-		oddStart = evens * 3;
-		cout << "\n\n    There is/are " << evens << 
-			" even uncommon digits.";
-
-		for (i = 2; i < oddStart; i += 2) {
-			cout << "\n    The digit " << *(returnedArray + i) 
-				<< " occurs " << *(returnedArray + i + 1) 
-				<< " time(s).";
-		}
-	} else {
-		oddStart = 2;
-	}
-
-
-
-	if (odds > 0) {
-		cout << "\n\n    There is/are " << odds <<
-			" odd uncommon digits.";
-		for (i = oddStart + 1; i < *(returnedArray); i += 2) {
-			cout << "\n    The digit " << *(returnedArray + i) 
-				<< " occurs " << *(returnedArray + i + 1) 
-				<< " time(s).";
-		}
-	}
-
-
-	delete[] userArray;
-	delete[] returnedArray;
-}
 
 int* getUncommonDigitStatsticsPeteWilcox(int* input, int size) {
 	bool isCommon[10] = {false};
@@ -172,7 +172,7 @@ int* getUncommonDigitStatsticsPeteWilcox(int* input, int size) {
 				// If we haven't looked already
 				if (isCommon[currentDigit] == false) {
 					// If it has not already been found common
-					for (j = 0; j < size && 
+					for (j = 0; j < size &&
 						isCommon[currentDigit] == false; j++) {
 						//Search each other integer
 						if (j != i) {
@@ -191,8 +191,8 @@ int* getUncommonDigitStatsticsPeteWilcox(int* input, int size) {
 
 								searchInt /= 10;
 
-							} while ((searchInt > 0) && 
-								(isCommon[currentDigit] == 
+							} while ((searchInt > 0) &&
+								(isCommon[currentDigit] ==
 									false));
 						}
 					}
@@ -266,7 +266,8 @@ Class Information --
 Assignment Information --
   Assignment Number:  Midterm #1 û Problem #2
   Written by:         Pete Wilcox
-  Due Date:           2015/10/22
+  Due Date:           2015/10/23
+
 
 
 ****************************************************
@@ -285,19 +286,21 @@ Select an option (1 or 2): 1
     32965
     -275721
     3028063
+
   Calling getUncommonDigitStatsticsPeteWilcox() -
 
   Displaying after returning the array --
     There is/are 5 uncommon digit(s)
 
     There is/are 2 even uncommon digits.
-    The digit 0 occurs 2 time(s).
-    The digit 8 occurs 1 time(s).
+      The digit 0 occurs 2 time(s).
+      The digit 8 occurs 1 time(s).
 
     There is/are 3 odd uncommon digits.
-    The digit 1 occurs 1 time(s).
-    The digit 7 occurs 2 time(s).
-    The digit 9 occurs 1 time(s).
+      The digit 1 occurs 1 time(s).
+      The digit 7 occurs 2 time(s).
+      The digit 9 occurs 1 time(s).
+
 ****************************************************
 *                     MENU                         *
 * 1. Calling getUncommonDigitStatsticsPeteWilcox() *
@@ -316,10 +319,12 @@ Select an option (1 or 2): 1
     -275721
     3028063
     10789
+
   Calling getUncommonDigitStatsticsPeteWilcox() -
 
   Displaying after returning the array --
     There is/are no uncommon digit(s)
+
 ****************************************************
 *                     MENU                         *
 * 1. Calling getUncommonDigitStatsticsPeteWilcox() *
@@ -328,28 +333,30 @@ Select an option (1 or 2): 1
 Select an option (1 or 2): 1
 
   How many integers? 3
-    Enter integer #1: 12548
-    Enter integer #2: 336548
-    Enter integer #3: 7789954
+    Enter integer #1: 3296
+    Enter integer #2: -2757
+    Enter integer #3: 107
 
   The original array:
-    12548
-    336548
-    7789954
+    3296
+    -2757
+    107
+
   Calling getUncommonDigitStatsticsPeteWilcox() -
 
   Displaying after returning the array --
     There is/are 6 uncommon digit(s)
 
     There is/are 2 even uncommon digits.
-    The digit 2 occurs 1 time(s).
-    The digit 6 occurs 1 time(s).
+      The digit 0 occurs 1 time(s).
+      The digit 6 occurs 1 time(s).
 
     There is/are 4 odd uncommon digits.
-    The digit 1 occurs 1 time(s).
-    The digit 3 occurs 2 time(s).
-    The digit 7 occurs 2 time(s).
-    The digit 9 occurs 2 time(s).
+      The digit 1 occurs 1 time(s).
+      The digit 3 occurs 1 time(s).
+      The digit 5 occurs 1 time(s).
+      The digit 9 occurs 1 time(s).
+
 ****************************************************
 *                     MENU                         *
 * 1. Calling getUncommonDigitStatsticsPeteWilcox() *
@@ -358,6 +365,7 @@ Select an option (1 or 2): 1
 Select an option (1 or 2): 4
 
   WRONG OPTION!
+
 ****************************************************
 *                     MENU                         *
 * 1. Calling getUncommonDigitStatsticsPeteWilcox() *
@@ -365,7 +373,7 @@ Select an option (1 or 2): 4
 ****************************************************
 Select an option (1 or 2): 2
 
-  Fun exercize ...
+  Fun exercise ...
 */
 
 /* COMMENTS:
@@ -390,3 +398,105 @@ I also forgot to set the first element of the array to actually
 contain the number of elements - I created the variable but
 neglected to assign it.
 */
+
+/* ORIGINAL CODE:
+
+ int* getUncommonDigitStatisticsPeteWilcox(int* input, int size) {
+	 bool isCommon[10] = {false};
+	 int digitCounter[10] = {0};
+	 int* uncommonArray;
+	 int newSize;
+	 int evens;
+	 int odds;
+	 int currentInt;
+	 int currentDigit;
+	 int searchDigit;
+	 int searchInt;
+	 int i, j;
+
+	 for (i = 0; i < size; i++) {
+		 currentInt = *(input + i);
+
+		 if (currentInt < 0) {
+			 currentInt = -currentInt;
+		 }
+
+		 do {
+			 currentDigit = currentInt % 10;
+
+			 if (digitCounter[currentDigit] == 0) {
+				 if (isCommon[currentDigit] == false) {
+					 for (j = 0; j < size; j++) {
+						 if (j != 0) {
+							 searchInt = *(input + j);
+							 if (searchInt < 0) {
+								 searchInt = -searchInt;
+							 }
+
+							 do {
+								 searchDigit = searchInt % 10;
+								 if (currentDigit == searchDigit) {
+									 isCommon[currentDigit] = true;
+								 }
+								 searchInt /= 10;
+							 } while ((searchInt > 0) && (isCommon[currentDigit] == true));
+
+						 }
+					 }
+				 }
+				 if (isCommon[currentDigit] == false) {
+					 digitCounter[currentDigit]++;
+				 }
+			 } else {
+				 isCommon[currentDigit]++;
+			 }
+			 currentInt /= 10;
+		 } while (currentInt > 0);
+	 }
+
+	 for (i = 0; i < 10; i++) {
+		 if (digitCounter[i] > 0) {
+			 if (digitCounter[i] % 2 == 0) {
+				 evens++;
+			 } else {
+				 odds++;
+			 }
+		 }
+	 }
+
+	 newSize = (3 + (evens * 2) + (odds * 2));
+	 uncommonArray = new int[newSize];
+
+	 *(uncommonArray) = newSize;
+
+	 j = 2;
+
+	 if (evens > 0) {
+		 *(uncommonArray + i) = evens;
+		 for (i = 0; i < 10; i++) {
+			 if (digitCounter[i] > 0) {
+				 *(uncommonArray + j) = i;
+				 *(uncommonArray + j + 1) = digitCounter[i];
+				 j += 2;
+			 }
+		 }
+	 } else {
+		 *(uncommonArray + i) = 0;
+	 }
+
+	 *(uncommonArray + j) = odds;
+	 j++;
+
+	 if (odds > 0) {
+		 for (i = 1; i < 10; i += 2) {
+			 if (digitCounter[i] > 0) {
+				 *(uncommonArray + j) = i;
+				 *(uncommonArray + j + 1) = digitCounter[i];
+				 j += 2;
+			 }
+		 }
+	 }
+
+	 return uncommonArray;
+ }
+ */
