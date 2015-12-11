b5627155a3a338fbf1009df314a7be4f8d70af2c
pcwilcox <petercwilcox@gmail.com>
2015-12-11 03:21:55 -0800

    Program output, user comments

--- CIS25Fall2015PeteWilcoxLab6Ex2/CIS25Fall2015PeteWilcoxLab6Ex2/CIS25Fall2015PeteWilcoxLab7Ex1.cpp
+++ CIS25Fall2015PeteWilcoxLab6Ex2/CIS25Fall2015PeteWilcoxLab6Ex2/CIS25Fall2015PeteWilcoxLab7Ex1.cpp
@@ -30,3 +30,841 @@ int main() {
 	return 0;
 }
 
+/* PROGRAM OUTPUT:
+CIS 25 - C++ Programming
+Laney College
+Pete Wilcox
+
+Assignment Information --
+  Assignment Number:  Lab 7,
+                      Exercise #1
+  Written by:         Pete Wilcox
+  Due Date:           2015/12/15
+
+MAIN MENU
+(1) Rectangle Tasks
+(2) Circle Tasks
+(3) Mixed Rectangle and Circle Tasks
+(4) Quit
+
+Enter your option (1, 2, 3, or 4): 1
+
+RECTANGLE MENU
+(1) Create two Rectangle objects
+(2) Compare by volume
+(3) Compare by area
+(4) Print two Rectangle objects
+(5) Quit
+Enter your option (1 through 5): 1
+
+  CREATING RECTANGLES MENU
+(1) Create first rectangle
+(2) Create second rectangle
+(3) Quit
+Please enter your option (1 through 3): 1
+
+Creating first rectangle --
+  Creating lower left point --
+    X value --
+      Please enter the numerator: 1
+      Please enter the denominator: 2
+
+Calling Fraction() on 1/2
+    Y value --
+      Please enter the numerator: 2
+      Please enter the denominator: 1
+
+Calling Fraction() on 2/1
+Calling Fraction() on 1/2
+Calling Fraction() on 2/1
+Calling PointPeteW() on (1/2,2/1)
+  Creating upper right point --
+    X value --
+      Please enter the numerator: 4
+      Please enter the denominator: 1
+
+    Y value --
+      Please enter the numerator: 1
+      Please enter the denominator: 1
+
+Calling Fraction() on 4/1
+Calling Fraction() on 1/1
+Calling PointPeteW() on (4/1,1/1)
+Calling Fraction() on 0/1
+Calling Fraction() on 4/1
+Calling Fraction() on 1/1
+Calling PointPeteW() on (4/1,1/1)
+Calling Fraction() on 1/2
+Calling Fraction() on 2/1
+Calling PointPeteW() on (1/2,2/1)
+Calling Fraction() on 4/1
+Calling Fraction() on 1/2
+Calling ~Fraction() on 1/2
+Calling ~Fraction() on 4/1
+Calling Fraction() on 1/1
+Calling Fraction() on 2/1
+Calling ~Fraction() on 2/1
+Calling ~Fraction() on 1/1
+Calling Fraction() on 2/1
+Calling Fraction() on 4/1
+Calling ~Fraction() on 4/1
+Calling ~Fraction() on 2/1
+Calling Fraction() on 4/1
+Calling Fraction() on 2/1
+Calling Fraction() on -2/1
+Calling Fraction() on 2/1
+Calling Fraction() on 1/1
+Calling Fraction() on -1/1
+Calling Fraction() on 2/1
+Calling ~Fraction() on 2/1
+Calling ~Fraction() on -1/1
+Calling ~Fraction() on 1/1
+Calling ~Fraction() on 2/1
+Calling ~Fraction() on -2/1
+Calling ~Fraction() on 2/1
+Calling ~Fraction() on 4/1
+Calling RectanglePeteW() on
+Rectangle:
+  Lower Left: (4/1,2/1)
+  Upper Right: (2/1,1/1)
+  Area: 2/1
+
+Rectangle #1 created:
+
+Rectangle:
+  Lower Left: (4/1,2/1)
+  Upper Right: (2/1,1/1)
+  Area: 2/1
+
+Calling ~Fraction() on 4/1
+Calling ~Fraction() on 1/1
+Calling ~PointPeteW() on (1/2,2/1)
+Calling ~Fraction() on 2/1
+Calling ~Fraction() on 1/2
+Calling ~PointPeteW() on (4/1,1/1)
+Calling ~Fraction() on 1/1
+Calling ~Fraction() on 4/1
+  CREATING RECTANGLES MENU
+(1) Create first rectangle
+(2) Create second rectangle
+(3) Quit
+Please enter your option (1 through 3): 2
+
+Creating second rectangle --
+  Creating lower left point --
+    X value --
+      Please enter the numerator: -1
+      Please enter the denominator: 1
+
+Calling Fraction() on -1/1
+    Y value --
+      Please enter the numerator: -1
+      Please enter the denominator: 2
+
+Calling Fraction() on -1/2
+Calling Fraction() on -1/1
+Calling Fraction() on -1/2
+Calling PointPeteW() on (-1/1,-1/2)
+  Creating upper right point --
+    X value --
+      Please enter the numerator: 2
+      Please enter the denominator: 1
+
+    Y value --
+      Please enter the numerator: -2
+      Please enter the denominator: 1
+
+Calling Fraction() on 2/1
+Calling Fraction() on -2/1
+Calling PointPeteW() on (2/1,-2/1)
+Calling Fraction() on 0/1
+Calling Fraction() on 2/1
+Calling Fraction() on -2/1
+Calling PointPeteW() on (2/1,-2/1)
+Calling Fraction() on -1/1
+Calling Fraction() on -1/2
+Calling PointPeteW() on (-1/1,-1/2)
+Calling Fraction() on 2/1
+Calling Fraction() on -1/1
+Calling ~Fraction() on -1/1
+Calling ~Fraction() on 2/1
+Calling Fraction() on -2/1
+Calling Fraction() on -1/2
+Calling ~Fraction() on -1/2
+Calling ~Fraction() on -2/1
+Calling Fraction() on -1/2
+Calling Fraction() on 2/1
+Calling ~Fraction() on 2/1
+Calling ~Fraction() on -1/2
+Calling Fraction() on 2/1
+Calling Fraction() on -1/2
+Calling Fraction() on -5/2
+Calling Fraction() on -1/2
+Calling Fraction() on -2/1
+Calling Fraction() on -3/2
+Calling Fraction() on 15/4
+Calling ~Fraction() on 15/4
+Calling ~Fraction() on -3/2
+Calling ~Fraction() on -2/1
+Calling ~Fraction() on -1/2
+Calling ~Fraction() on -5/2
+Calling ~Fraction() on -1/2
+Calling ~Fraction() on 2/1
+Calling RectanglePeteW() on
+Rectangle:
+  Lower Left: (2/1,-1/2)
+  Upper Right: (-1/2,-2/1)
+  Area: 15/4
+
+Rectangle #2 created:
+
+Rectangle:
+  Lower Left: (2/1,-1/2)
+  Upper Right: (-1/2,-2/1)
+  Area: 15/4
+
+Calling ~Fraction() on 2/1
+Calling ~Fraction() on -2/1
+Calling ~PointPeteW() on (-1/1,-1/2)
+Calling ~Fraction() on -1/2
+Calling ~Fraction() on -1/1
+Calling ~PointPeteW() on (2/1,-2/1)
+Calling ~Fraction() on -2/1
+Calling ~Fraction() on 2/1
+  CREATING RECTANGLES MENU
+(1) Create first rectangle
+(2) Create second rectangle
+(3) Quit
+Please enter your option (1 through 3): 3
+
+Returning to main menu.
+RECTANGLE MENU
+(1) Create two Rectangle objects
+(2) Compare by volume
+(3) Compare by area
+(4) Print two Rectangle objects
+(5) Quit
+Enter your option (1 through 5): 2
+
+Rectangles have no volume.
+RECTANGLE MENU
+(1) Create two Rectangle objects
+(2) Compare by volume
+(3) Compare by area
+(4) Print two Rectangle objects
+(5) Quit
+Enter your option (1 through 5): 3
+
+Comparing area --
+
+Calling Fraction() on 15/4
+Calling Fraction() on 2/1
+Calling ~Fraction() on 2/1
+Calling ~Fraction() on 15/4
+Calling Fraction() on 15/4
+Calling Fraction() on 2/1
+Calling ~Fraction() on 2/1
+Calling ~Fraction() on 15/4
+Rectangle:
+  Lower Left: (2/1,-1/2)
+  Upper Right: (-1/2,-2/1)
+  Area: 15/4
+ is larger than
+Rectangle:
+  Lower Left: (4/1,2/1)
+  Upper Right: (2/1,1/1)
+  Area: 2/1
+
+RECTANGLE MENU
+(1) Create two Rectangle objects
+(2) Compare by volume
+(3) Compare by area
+(4) Print two Rectangle objects
+(5) Quit
+Enter your option (1 through 5): 4
+
+Rectangle:
+  Lower Left: (4/1,2/1)
+  Upper Right: (2/1,1/1)
+  Area: 2/1
+
+Rectangle:
+  Lower Left: (2/1,-1/2)
+  Upper Right: (-1/2,-2/1)
+  Area: 15/4
+
+RECTANGLE MENU
+(1) Create two Rectangle objects
+(2) Compare by volume
+(3) Compare by area
+(4) Print two Rectangle objects
+(5) Quit
+Enter your option (1 through 5): 5
+
+Returning to main menu.
+MAIN MENU
+(1) Rectangle Tasks
+(2) Circle Tasks
+(3) Mixed Rectangle and Circle Tasks
+(4) Quit
+
+Enter your option (1, 2, 3, or 4): 2
+
+CIRCLE MENU
+(1) Create two Circle objects
+(2) Compare by volume
+(3) Compare by area
+(4) Print two Circle objects
+(5) Quit
+Enter your option (1 through 5): 1
+
+  CREATING CIRCLES MENU --
+  (1) Create first circle
+  (2) Create second circle
+  (3) Quit
+Please select your option (1 through 3):1
+
+Creating first circle --
+  Creating center point --
+    X value:
+      Please enter the numerator: 1
+
+      Please enter the denominator: 2
+
+Calling Fraction() on 1/2
+    Y value:
+      Please enter the numerator: 2
+
+      Please enter the denominator: 1
+
+Calling Fraction() on 2/1
+Calling Fraction() on 1/2
+Calling Fraction() on 2/1
+Calling PointPeteW() on (1/2,2/1)
+    Creating radius --
+      Please enter the numerator: 4
+
+      Please enter the denominator: 1
+
+Calling Fraction() on 4/1
+Calling Fraction() on 0/1
+Calling Fraction() on 1/2
+Calling Fraction() on 2/1
+Calling PointPeteW() on (1/2,2/1)
+Calling Fraction() on 4/1
+Calling Fraction() on 157/50
+Calling Fraction() on 16/1
+Calling Fraction() on 2512/50
+Calling ~Fraction() on 1256/25
+Calling ~Fraction() on 16/1
+Calling ~Fraction() on 157/50
+Calling CirclePeteW() on
+Circle:
+  Center: (1/2,2/1)
+  Radius: 4/1
+  Area: 1256/25
+Circle #1 created:
+Circle:
+  Center: (1/2,2/1)
+  Radius: 4/1
+  Area: 1256/25
+Calling ~Fraction() on 1/2
+Calling ~Fraction() on 2/1
+Calling ~Fraction() on 4/1
+Calling ~PointPeteW() on (1/2,2/1)
+Calling ~Fraction() on 2/1
+Calling ~Fraction() on 1/2
+  CREATING CIRCLES MENU --
+  (1) Create first circle
+  (2) Create second circle
+  (3) Quit
+Please select your option (1 through 3):2
+
+Creating second circle --
+  Creating center point --
+    X value:
+      Please enter the numerator: -1
+
+      Please enter the denominator: 1
+
+Calling Fraction() on -1/1
+    Y value:
+      Please enter the numerator: -1
+
+      Please enter the denominator: 2
+
+Calling Fraction() on -1/2
+Calling Fraction() on -1/1
+Calling Fraction() on -1/2
+Calling PointPeteW() on (-1/1,-1/2)
+    Creating radius --
+      Please enter the numerator: 2
+
+      Please enter the denominator: 1
+
+Calling Fraction() on 2/1
+Calling Fraction() on 0/1
+Calling Fraction() on -1/1
+Calling Fraction() on -1/2
+Calling PointPeteW() on (-1/1,-1/2)
+Calling Fraction() on 2/1
+Calling Fraction() on 157/50
+Calling Fraction() on 4/1
+Calling Fraction() on 628/50
+Calling ~Fraction() on 314/25
+Calling ~Fraction() on 4/1
+Calling ~Fraction() on 157/50
+Calling CirclePeteW() on
+Circle:
+  Center: (-1/1,-1/2)
+  Radius: 2/1
+  Area: 314/25
+Circle #2 created:
+Circle:
+  Center: (-1/1,-1/2)
+  Radius: 2/1
+  Area: 314/25
+Calling ~Fraction() on -1/1
+Calling ~Fraction() on -1/2
+Calling ~Fraction() on 2/1
+Calling ~PointPeteW() on (-1/1,-1/2)
+Calling ~Fraction() on -1/2
+Calling ~Fraction() on -1/1
+  CREATING CIRCLES MENU --
+  (1) Create first circle
+  (2) Create second circle
+  (3) Quit
+Please select your option (1 through 3):3
+
+Returning to main menu.
+CIRCLE MENU
+(1) Create two Circle objects
+(2) Compare by volume
+(3) Compare by area
+(4) Print two Circle objects
+(5) Quit
+Enter your option (1 through 5): 2
+
+Both circles have zero volume.
+CIRCLE MENU
+(1) Create two Circle objects
+(2) Compare by volume
+(3) Compare by area
+(4) Print two Circle objects
+(5) Quit
+Enter your option (1 through 5): 3
+
+Comparing area --
+
+Calling Fraction() on 314/25
+Calling Fraction() on 1256/25
+Calling ~Fraction() on 1256/25
+Calling ~Fraction() on 314/25
+Circle:
+  Center: (1/2,2/1)
+  Radius: 4/1
+  Area: 1256/25 is larger than
+Circle:
+  Center: (-1/1,-1/2)
+  Radius: 2/1
+  Area: 314/25
+CIRCLE MENU
+(1) Create two Circle objects
+(2) Compare by volume
+(3) Compare by area
+(4) Print two Circle objects
+(5) Quit
+Enter your option (1 through 5): 4
+
+Circle:
+  Center: (1/2,2/1)
+  Radius: 4/1
+  Area: 1256/25
+Circle:
+  Center: (-1/1,-1/2)
+  Radius: 2/1
+  Area: 314/25
+CIRCLE MENU
+(1) Create two Circle objects
+(2) Compare by volume
+(3) Compare by area
+(4) Print two Circle objects
+(5) Quit
+Enter your option (1 through 5): 5
+
+Returning to main menu.
+MAIN MENU
+(1) Rectangle Tasks
+(2) Circle Tasks
+(3) Mixed Rectangle and Circle Tasks
+(4) Quit
+
+Enter your option (1, 2, 3, or 4): 3
+
+MIXED RECTANGLE & CIRCLE MENU
+(1) Compare by area
+(2) Compare by volume
+(3) Print ALL objects
+(4) Quit
+Enter your option (1 through 4): 1
+
+  Comparing by area - Select two objects to compare --
+Please select the first object:
+  (1) Rectangle 1
+  (2) Rectangle 2
+  (3) Circle 1
+  (4) Circle 2
+Please select your object (1 through 4): 1
+
+Please select the second object:
+  (1) Rectangle 1
+  (2) Rectangle 2
+  (3) Circle 1
+  (4) Circle 2
+Please select your object (1 through 4): 2
+
+Comparing area --
+
+Calling Fraction() on 15/4
+Calling Fraction() on 2/1
+Calling ~Fraction() on 2/1
+Calling ~Fraction() on 15/4
+Calling Fraction() on 15/4
+Calling Fraction() on 2/1
+Calling ~Fraction() on 2/1
+Calling ~Fraction() on 15/4
+Rectangle:
+  Lower Left: (2/1,-1/2)
+  Upper Right: (-1/2,-2/1)
+  Area: 15/4
+ is larger than
+Rectangle:
+  Lower Left: (4/1,2/1)
+  Upper Right: (2/1,1/1)
+  Area: 2/1
+
+MIXED RECTANGLE & CIRCLE MENU
+(1) Compare by area
+(2) Compare by volume
+(3) Print ALL objects
+(4) Quit
+Enter your option (1 through 4): 1
+
+  Comparing by area - Select two objects to compare --
+Please select the first object:
+  (1) Rectangle 1
+  (2) Rectangle 2
+  (3) Circle 1
+  (4) Circle 2
+Please select your object (1 through 4): 1
+
+Please select the second object:
+  (1) Rectangle 1
+  (2) Rectangle 2
+  (3) Circle 1
+  (4) Circle 2
+Please select your object (1 through 4): 3
+
+Comparing area --
+
+Calling Fraction() on 1256/25
+Calling Fraction() on 2/1
+Calling ~Fraction() on 2/1
+Calling ~Fraction() on 1256/25
+Calling Fraction() on 1256/25
+Calling Fraction() on 2/1
+Calling ~Fraction() on 2/1
+Calling ~Fraction() on 1256/25
+Circle:
+  Center: (1/2,2/1)
+  Radius: 4/1
+  Area: 1256/25 is larger than
+Rectangle:
+  Lower Left: (4/1,2/1)
+  Upper Right: (2/1,1/1)
+  Area: 2/1
+
+MIXED RECTANGLE & CIRCLE MENU
+(1) Compare by area
+(2) Compare by volume
+(3) Print ALL objects
+(4) Quit
+Enter your option (1 through 4): 1
+
+  Comparing by area - Select two objects to compare --
+Please select the first object:
+  (1) Rectangle 1
+  (2) Rectangle 2
+  (3) Circle 1
+  (4) Circle 2
+Please select your object (1 through 4): 1
+
+Please select the second object:
+  (1) Rectangle 1
+  (2) Rectangle 2
+  (3) Circle 1
+  (4) Circle 2
+Please select your object (1 through 4): 4
+
+Comparing area --
+
+Calling Fraction() on 314/25
+Calling Fraction() on 2/1
+Calling ~Fraction() on 2/1
+Calling ~Fraction() on 314/25
+Calling Fraction() on 314/25
+Calling Fraction() on 2/1
+Calling ~Fraction() on 2/1
+Calling ~Fraction() on 314/25
+Circle:
+  Center: (-1/1,-1/2)
+  Radius: 2/1
+  Area: 314/25 is larger than
+Rectangle:
+  Lower Left: (4/1,2/1)
+  Upper Right: (2/1,1/1)
+  Area: 2/1
+
+MIXED RECTANGLE & CIRCLE MENU
+(1) Compare by area
+(2) Compare by volume
+(3) Print ALL objects
+(4) Quit
+Enter your option (1 through 4): 1
+
+  Comparing by area - Select two objects to compare --
+Please select the first object:
+  (1) Rectangle 1
+  (2) Rectangle 2
+  (3) Circle 1
+  (4) Circle 2
+Please select your object (1 through 4): 2
+
+Please select the second object:
+  (1) Rectangle 1
+  (2) Rectangle 2
+  (3) Circle 1
+  (4) Circle 2
+Please select your object (1 through 4): 3
+
+Comparing area --
+
+Calling Fraction() on 1256/25
+Calling Fraction() on 15/4
+Calling ~Fraction() on 15/4
+Calling ~Fraction() on 1256/25
+Calling Fraction() on 1256/25
+Calling Fraction() on 15/4
+Calling ~Fraction() on 15/4
+Calling ~Fraction() on 1256/25
+Circle:
+  Center: (1/2,2/1)
+  Radius: 4/1
+  Area: 1256/25 is larger than
+Rectangle:
+  Lower Left: (2/1,-1/2)
+  Upper Right: (-1/2,-2/1)
+  Area: 15/4
+
+MIXED RECTANGLE & CIRCLE MENU
+(1) Compare by area
+(2) Compare by volume
+(3) Print ALL objects
+(4) Quit
+Enter your option (1 through 4): 1
+
+  Comparing by area - Select two objects to compare --
+Please select the first object:
+  (1) Rectangle 1
+  (2) Rectangle 2
+  (3) Circle 1
+  (4) Circle 2
+Please select your object (1 through 4): 2
+
+Please select the second object:
+  (1) Rectangle 1
+  (2) Rectangle 2
+  (3) Circle 1
+  (4) Circle 2
+Please select your object (1 through 4): 4
+
+Comparing area --
+
+Calling Fraction() on 314/25
+Calling Fraction() on 15/4
+Calling ~Fraction() on 15/4
+Calling ~Fraction() on 314/25
+Calling Fraction() on 314/25
+Calling Fraction() on 15/4
+Calling ~Fraction() on 15/4
+Calling ~Fraction() on 314/25
+Circle:
+  Center: (-1/1,-1/2)
+  Radius: 2/1
+  Area: 314/25 is larger than
+Rectangle:
+  Lower Left: (2/1,-1/2)
+  Upper Right: (-1/2,-2/1)
+  Area: 15/4
+
+MIXED RECTANGLE & CIRCLE MENU
+(1) Compare by area
+(2) Compare by volume
+(3) Print ALL objects
+(4) Quit
+Enter your option (1 through 4): 1
+
+  Comparing by area - Select two objects to compare --
+Please select the first object:
+  (1) Rectangle 1
+  (2) Rectangle 2
+  (3) Circle 1
+  (4) Circle 2
+Please select your object (1 through 4): 3
+
+Please select the second object:
+  (1) Rectangle 1
+  (2) Rectangle 2
+  (3) Circle 1
+  (4) Circle 2
+Please select your object (1 through 4): 4
+
+Comparing area --
+
+Calling Fraction() on 314/25
+Calling Fraction() on 1256/25
+Calling ~Fraction() on 1256/25
+Calling ~Fraction() on 314/25
+Circle:
+  Center: (1/2,2/1)
+  Radius: 4/1
+  Area: 1256/25 is larger than
+Circle:
+  Center: (-1/1,-1/2)
+  Radius: 2/1
+  Area: 314/25
+MIXED RECTANGLE & CIRCLE MENU
+(1) Compare by area
+(2) Compare by volume
+(3) Print ALL objects
+(4) Quit
+Enter your option (1 through 4): 3
+
+Rectangle:
+  Lower Left: (4/1,2/1)
+  Upper Right: (2/1,1/1)
+  Area: 2/1
+
+Rectangle:
+  Lower Left: (2/1,-1/2)
+  Upper Right: (-1/2,-2/1)
+  Area: 15/4
+
+Circle:
+  Center: (1/2,2/1)
+  Radius: 4/1
+  Area: 1256/25
+Circle:
+  Center: (-1/1,-1/2)
+  Radius: 2/1
+  Area: 314/25
+MIXED RECTANGLE & CIRCLE MENU
+(1) Compare by area
+(2) Compare by volume
+(3) Print ALL objects
+(4) Quit
+Enter your option (1 through 4): 2
+
+Rectangles and circles have no volume.
+MIXED RECTANGLE & CIRCLE MENU
+(1) Compare by area
+(2) Compare by volume
+(3) Print ALL objects
+(4) Quit
+Enter your option (1 through 4): 4
+
+Returning to main menu.
+MAIN MENU
+(1) Rectangle Tasks
+(2) Circle Tasks
+(3) Mixed Rectangle and Circle Tasks
+(4) Quit
+
+Enter your option (1, 2, 3, or 4): 5
+
+
+    WRONG OPTION
+MAIN MENU
+(1) Rectangle Tasks
+(2) Circle Tasks
+(3) Mixed Rectangle and Circle Tasks
+(4) Quit
+
+Enter your option (1, 2, 3, or 4): 4
+
+
+    Have fun...
+Calling ~RectanglePeteW() on
+Rectangle:
+  Lower Left: (4/1,2/1)
+  Upper Right: (2/1,1/1)
+  Area: 2/1
+
+Calling ~PointPeteW() on (4/1,2/1)
+Calling ~Fraction() on 2/1
+Calling ~Fraction() on 4/1
+Calling ~PointPeteW() on (2/1,1/1)
+Calling ~Fraction() on 1/1
+Calling ~Fraction() on 2/1
+Calling ~Fraction() on 2/1
+Calling ~RectanglePeteW() on
+Rectangle:
+  Lower Left: (2/1,-1/2)
+  Upper Right: (-1/2,-2/1)
+  Area: 15/4
+
+Calling ~PointPeteW() on (2/1,-1/2)
+Calling ~Fraction() on -1/2
+Calling ~Fraction() on 2/1
+Calling ~PointPeteW() on (-1/2,-2/1)
+Calling ~Fraction() on -2/1
+Calling ~Fraction() on -1/2
+Calling ~Fraction() on 15/4
+Calling ~CirclePeteW() on
+Circle:
+  Center: (1/2,2/1)
+  Radius: 4/1
+  Area: 1256/25
+Calling ~Fraction() on 4/1
+Calling ~PointPeteW() on (1/2,2/1)
+Calling ~Fraction() on 2/1
+Calling ~Fraction() on 1/2
+Calling ~Fraction() on 1256/25
+Calling ~CirclePeteW() on
+Circle:
+  Center: (-1/1,-1/2)
+  Radius: 2/1
+  Area: 314/25
+Calling ~Fraction() on 2/1
+Calling ~PointPeteW() on (-1/1,-1/2)
+Calling ~Fraction() on -1/2
+Calling ~Fraction() on -1/1
+Calling ~Fraction() on 314/25
+*/
+
+/* COMMENTS:
+2015/12/15:
+
+This was a good opportunity to use Shape() as an abstract base
+class for Circle() and Rectangle(). It allowed me to
+significantly reduce the amount of code necessary to perform
+some manipulations, particularly when it came to comparing the
+different shapes against each other.
+
+The requirement to print each constructor/destructor call makes
+the code look pretty ugly, and it is easy to think that you
+should try to create these objects without creating the member
+data objects first. I believe this is the wrong mindset though
+as it would violate the principles of abstraction.
+
+*/
